85dde4d26df66f585e9ce450ea21acf1dbdddefc
martamasramon <marta.masramon@gmail.com>
2023-01-30 11:50:30 +0000

    Increased memory in jobs

--- train.sh
+++ train.sh
@@ -1,4 +1,4 @@
-#$ -l tmem=16G,h_vmem=16G
+#$ -l tmem=32G,h_vmem=32G
 #$ -l h_rt=2:00:00
 #$ -l gpu=true
 
@@ -15,7 +15,7 @@ date
 export PATH=/share/apps/python-3.6.9-tkinter/bin:$PATH
 export LD_LIBRARY_PATH=/share/apps/python-3.6.9-tkinter/lib:$LD_LIBRARY_PATH
 
-python3 train.py --geometric-model affine
+#python3 train.py --geometric-model affine
 python3 train.py --geometric-model tps
 
 date
