888588f725b2afabab6b0fd6646ecd0dbb8d4bf9
martamasramon <marta.masramon@gmail.com>
2023-01-30 11:50:30 +0000

    Increased memory in jobs

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
-#$ -l tmem=16G,h_vmem=16G
+#$ -l tmem=32G,h_vmem=32G
 #$ -l h_rt=2:00:00
 #$ -l gpu=true
 
@@ -15,7 +15,7 @@ date
 export PATH=/share/apps/python-3.6.9-tkinter/bin:$PATH
 export LD_LIBRARY_PATH=/share/apps/python-3.6.9-tkinter/lib:$LD_LIBRARY_PATH
 
-python3 register_images.py -i jsonData/TCIA_FUSION.json -pm -pf -r -v
+python3 register_images.py -i jsonData/TCIA_FUSION.json -r -v #-pm -pf
 
 date
 
