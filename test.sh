c57a6bc959e9031bf106a19643c706d99b65427e
martamasramon <marta.masramon@gmail.com>
2023-02-06 18:41:35 +0000

    explain additional inputs in register_images.py

--- test.sh
+++ test.sh
@@ -15,7 +15,8 @@ date
 export PATH=/share/apps/python-3.6.9-tkinter/bin:$PATH
 export LD_LIBRARY_PATH=/share/apps/python-3.6.9-tkinter/lib:$LD_LIBRARY_PATH
 
-python3 register_images.py -i jsonData/TCIA_FUSION.json -r -v #-pm -pf
+python3 register_images.py -i jsonData/TCIA_FUSION.json -v -r
+# -v: verbose, -pf: process fixed (MRI), -pm: process moving (histo), -r: register
 
 date
 
