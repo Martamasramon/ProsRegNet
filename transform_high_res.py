d78a43465680ea73dc9a8f0ba024789d2a7379ce
martamasramon <marta.masramon@gmail.com>
2023-03-16 22:22:08 +0000

    apply saved transformation to high res histo

---
+++ transform_high_res.py
@@ -0,0 +1,99 @@
+import json
+from process_img import * 
+from geotnf.transformation_high_res import GeometricTnf_high_res
+from skimage import io
+from glob import glob
+from register_images import output_results
+
+def get_data(filename): 
+    with open(filename) as file:
+        file_contents = file.read()
+    
+    json_data = json.loads(file_contents)
+    
+    return json_data
+    
+
+def transform_high_res(transforms, path, use_cuda=True):
+    # Get transforms
+    theta_aff_1, theta_aff_2, theta_tps = transforms
+        
+    # Load geometric models
+    tpsTnf_high_res = GeometricTnf_high_res(geometric_model='tps',    use_cuda=use_cuda)
+    affTnf_high_res = GeometricTnf_high_res(geometric_model='affine', use_cuda=use_cuda)
+    
+    # Preprocess image 
+    source_image     = io.imread(path)
+    source_image_var = process_image(source_image, use_cuda, half_out_size=1024, high_res=True)
+
+    # Affine transformations
+    warped_image_aff = affTnf_high_res(source_image_var, theta_aff_1.view(-1,2,3))
+    warped_image_aff = affTnf_high_res(warped_image_aff, theta_aff_2.view(-1,2,3))
+   
+    # TPS transformation
+    warped_image_aff_tps = tpsTnf_high_res(warped_image_aff,theta_tps)
+     
+    # Un-normalize images and convert to numpy
+    warped_image_aff_tps_np = normalize_image(warped_image_aff_tps,forward=False).data.squeeze(0).transpose(0,1).transpose(1,2).cpu().numpy()
+    
+    # Ignore negative values
+    warped_image_aff_tps_np[warped_image_aff_tps_np < 0] = 0    
+   
+    return warped_image_aff_tps_np
+
+
+path    = "./results/preprocess/"
+samples = ["HMU_010_FH"]
+
+def main():
+    for sid in samples:
+        json_path  = 'transform_' + sid + '.json'
+        img_path   = os.path.join(path, 'hist' , sid+'_high_res/')
+        
+        # Get image paths
+        all_paths = {}
+        all_paths['histo'] = sorted(glob(img_path + 'hist*.png'))
+        all_paths['mask']  = sorted(glob(img_path + 'mask*.png'))
+        
+        json_data = get_data(json_path)
+        
+        # Get sid params
+        origin      = json_data["origin"]
+        direction   = json_data["direction"]
+        spacing     = json_data["spacing"]
+        
+        scale       = json_data["scale"]
+        hist_space  = [spacing[0]/scale[0], spacing[1]/scale[1], spacing[2]]
+        
+        spatialInfo = (origin, hist_space, direction)
+        
+        # Get image size
+        img     = io.imread(all_paths['histo'][0])
+        w, h, _ = img.shape
+        count   = len(all_paths['histo'])
+        
+        warped_imgs = {}
+        for annot in all_paths:
+            warped_imgs[annot] = np.zeros((count, w, h, 3))
+        
+        # Get transformation params
+        for i in range(count):
+            theta_aff_1 = json_data[i]["affine_1"]
+            theta_aff_2 = json_data[i]["affine_2"]
+            theta_tps   = json_data[i]["tps"]
+            transforms  = (theta_aff_1, theta_aff_2, theta_tps)
+            
+            for annot in all_paths:
+                warped_imgs[annot][i,:,:,:] = transform_high_res(transforms, all_paths[annot][i], use_cuda=True)
+            
+            # Apply mask to image
+            mask = np.zeros((w, h, 3), dtype=int)
+            for n in range(3):
+                mask[:, :, n]  = warped_imgs['mask'][i, :, :, 0] 
+            warped_imgs['histo'][i,:,:,:] = np.multiply(warped_imgs['histo'][i,:,:,:],mask)
+            
+        # Save images
+        for annot in all_paths:
+            output_results(path + 'registration/', warped_imgs[annot], sid + '_high_res', annot, spatialInfo, extension = 'nii.gz')
+        
+main()
