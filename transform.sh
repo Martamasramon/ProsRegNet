089ff9a19b4831c2f8cafe8409488593419e15a1
martamasramon <marta.masramon@gmail.com>
2023-03-20 00:12:35 +0000

    fixed typo

--- transform.sh
+++ transform.sh
@@ -1,4 +1,4 @@
-#$ -l tmem=32G,h_vmem=32G
+#$ -l tmem=128G,h_vmem=128G
 #$ -l h_rt=2:00:00
 #$ -l gpu=true
 
@@ -15,6 +15,6 @@ date
 export PATH=/share/apps/python-3.6.9-tkinter/bin:$PATH
 export LD_LIBRARY_PATH=/share/apps/python-3.6.9-tkinter/lib:$LD_LIBRARY_PATH
 
-python3 transorm_high_res.py 
+python3 transform_high_res.py 
 
 date
