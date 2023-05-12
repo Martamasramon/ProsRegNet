7dbac14c3c1b8d8407debfb305c75de2c179328a
Martamasramon <Marta.masramon@gmail.com>
2023-05-12 11:17:07 +0200

    updated training data

--- train.sh
+++ train.sh
@@ -15,7 +15,7 @@ date
 export PATH=/share/apps/python-3.6.9-tkinter/bin:$PATH
 export LD_LIBRARY_PATH=/share/apps/python-3.6.9-tkinter/lib:$LD_LIBRARY_PATH
 
-python3 train.py --geometric-model affine
-python3 train.py --geometric-model tps
+python3 train.py --geometric-model affine -n 'extended_T2W'
+python3 train.py --geometric-model tps    -n 'extended_T2W'
 
 date
