7d985ec149687b1fdad565a1ef3e494ab76caa50
Martamasramon <Marta.masramon@gmail.com>
2023-05-19 16:02:17 +0100

    landmark dataset

--- train_landmarks.py
+++ train_landmarks.py
@@ -21,6 +21,7 @@ from image.normalization        import NormalizeImageDict
 from util.torch_util            import save_checkpoint
 from util.train_test_fn         import test
 from data.synth_dataset         import SynthDataset
+from data.landmark_dataset      import LandmarkDataset
 from geotnf.transformation      import SynthPairTnf
 from collections                import OrderedDict
 from torch.optim.lr_scheduler   import StepLR
@@ -32,99 +33,6 @@ from torch.autograd             import Variable
 # Ignore warnings
 import warnings
 warnings.simplefilter("ignore", UserWarning)
-
-class LandmarkDataset(Dataset):
-    """    
-    Args:
-            csv_file (string): Path to the csv file with image names and landmarks.
-            training_image_path (string): Directory with all the images.
-            transform (callable): Transformation for post-processing the training pair (eg. image normalization)
-            
-    Returns:
-            Dict: {'histo': histo image, 'MRI': mri image, 'source_landmarks': landmark location on histo, 'target_landmarks': landmark location on mri}
-            
-    """
-
-    def __init__(self, csv_file, training_image_path, output_size=(240,240), geometric_model='tps', transform=None,
-                 random_sample=False, random_t=0.5, random_s=0.5, random_alpha=1/6, random_t_tps=0.4):
-        
-        # random_sample is used to indicate whether deformation coefficients are randomly generated?
-        self.random_sample      = random_sample
-        self.random_t           = random_t
-        self.random_t_tps       = random_t_tps
-        self.random_alpha       = random_alpha
-        self.random_s           = random_s
-        self.out_h, self.out_w  = output_size
-        
-        # read csv file
-        self.train_data         = pd.read_csv(csv_file)
-        self.img_histo_names    = self.train_data.iloc[:,0]
-        self.img_MRI_names      = self.train_data.iloc[:,1]
-        self.histo_x            = self.train_data.iloc[:,2]
-        self.histo_y            = self.train_data.iloc[:,3]
-        self.MRI_x              = self.train_data.iloc[:,4]
-        self.MRI_y              = self.train_data.iloc[:,5]
-        
-        # copy arguments
-        self.training_image_path    = training_image_path
-        self.transform              = transform
-        self.geometric_model        = geometric_model
-        
-        # affine transform used to rescale images
-        self.affineTnf = GeometricTnf(out_h=self.out_h, out_w=self.out_w, use_cuda = False) 
-        
-    def __len__(self):
-        return len(self.train_data)
-
-    def __getitem__(self, idx):
-        # read image
-        img_histo_name  = os.path.join(self.training_image_path, self.img_histo_names[idx])
-        image_histo     = io.imread(img_histo_name)
-
-        img_MRI_name  = os.path.join(self.training_image_path, self.img_MRI_names[idx])
-        MRI           = io.imread(img_MRI_name)
-        image_MRI     = np.zeros((MRI.shape[0],MRI.shape[1],3))
-        for i in range(3):
-            image_MRI[:,:,i] = MRI
-        
-        # Get the landmarks in the correct format - as an int list
-        histo_x_array = [int(a) for a in self.histo_x[idx].split(';')]
-        histo_y_array = [int(a) for a in self.histo_y[idx].split(';')]
-
-        # Use the landmark coordinates to create histo 'landmarks image'
-        num_landmarks   = len(histo_x_array)
-        landmarks_histo = np.zeros((image_histo.shape[0], image_histo.shape[1], num_landmarks))
-        for i in range(num_landmarks):
-            landmarks_histo[histo_x_array[i],histo_y_array[i],i] = 1
-            
-        # For the target, keep landmark locations as array
-        landmarks_mri = np.zeros((2,num_landmarks))
-        landmarks_mri[0,:] = self.MRI_x[idx].split(';')
-        landmarks_mri[1,:] = self.MRI_y[idx].split(';')
-        
-        # make arrays float tensor for subsequent processing
-        image_histo     = torch.Tensor(image_histo.astype(np.float32))
-        image_MRI       = torch.Tensor(image_MRI.astype(np.float32))
-        landmarks_histo = torch.Tensor(landmarks_histo.astype(np.float32))
-        landmarks_mri   = torch.Tensor(landmarks_mri.astype(np.float32))
-
-        # permute order of image to CHW
-        image_histo = image_histo.transpose(1,2).transpose(0,1)
-        image_MRI   = image_MRI.transpose(1,2).transpose(0,1)
-                
-        # Resize image using bilinear sampling with identity affine tnf
-        if image_histo.size()[0]!=self.out_h or image_histo.size()[1]!=self.out_w:
-            image_histo = self.affineTnf(Variable(image_histo.unsqueeze(0),requires_grad=False)).data.squeeze(0)
-        if image_MRI.size()[0]!=self.out_h or image_MRI.size()[1]!=self.out_w:
-            image_MRI = self.affineTnf(Variable(image_MRI.unsqueeze(0),requires_grad=False)).data.squeeze(0)
-                
-        sample = {'source_image': image_histo, 'target_image': image_MRI, 'landmarks_source': landmarks_histo, 'landmarks_target':landmarks_mri}
-
-        if self.transform:
-            sample = self.transform(sample)
-
-        return sample
-
 
 class LandmarkTnf(object):
 
@@ -224,25 +132,6 @@ def train(epoch, model, loss_fn, optimizer, dataloader, landmark_tnf, use_cuda=T
     return train_loss
 
 
-def load_models(model_path, feature_extraction_cnn='resnet101'): 
-    """ 
-    Load pre-trained models
-    """
-    
-    use_cuda = torch.cuda.is_available()
-    
-    # Create model
-    print('Creating CNN model...')
-    model = ProsRegNet(use_cuda=use_cuda, geometric_model='tps',   feature_extraction_cnn=feature_extraction_cnn)
-
-    # Load trained weights
-    print('Loading trained model weights...')
-    checkpoint               = torch.load(model_path, map_location=lambda storage, loc: storage)
-    checkpoint['state_dict'] = OrderedDict([(k.replace('resnet101', 'model'), v) for k, v in checkpoint['state_dict'].items()])
-    model.load_state_dict(checkpoint['state_dict'])
-        
-    return (model, use_cuda)
-
 # Argument parsing
 parser = argparse.ArgumentParser(description='ProsRegNet PyTorch implementation')
 
@@ -252,7 +141,7 @@ parser.add_argument(      '--test-csv-name',       type=str, default='test.csv',
 
 parser.add_argument('-p', '--training-image-path', type=str, default='datasets/training_landmarks/',  help='path to folder containing training images')
 parser.add_argument(      '--trained-models-dir',  type=str, default='trained_models',                help='path to trained models folder')
-parser.add_argument('-n', '--trained-models-name', type=str, default='default',                       help='trained model filename')
+parser.add_argument('-n', '--trained-models-name', type=str, default='default',               help='trained model filename')
 
 # Optimization parameters 
 parser.add_argument('--lr',             type=float, default=0.0003, help='learning rate')
@@ -268,8 +157,8 @@ args     = parser.parse_args()
 ## CUDA
 use_cuda = torch.cuda.is_available() 
 print("Use Cuda? ", use_cuda)
-torch.cuda.set_device(0)
-print("cuda:", torch.cuda.current_device())
+#torch.cuda.set_device(0)
+#print("cuda:", torch.cuda.current_device())
 
 # Seed
 if use_cuda:
@@ -279,7 +168,8 @@ if use_cuda:
 print('Creating CNN model...')
 
 model       = ProsRegNet(use_cuda=use_cuda,geometric_model='tps',feature_extraction_cnn='resnet101')
-checkpoint  = torch.load(args.trained_models_name, map_location=lambda storage, loc: storage)
+model_path  = os.path.join(args.trained_models_dir, 'best_' + args.trained_models_name + '_tps.pth.tar')
+checkpoint  = torch.load(model_path, map_location=lambda storage, loc: storage)
 checkpoint['state_dict'] = OrderedDict([(k.replace('resnet101', 'model'), v) for k, v in checkpoint['state_dict'].items()])
 model.load_state_dict(checkpoint['state_dict'])
 
@@ -289,8 +179,9 @@ loss = nn.MSELoss()
 # Dataset and dataloader
 dataset_train = LandmarkDataset(geometric_model  = 'tps',
                             csv_file             = './training_data/tps/' + args.training_csv_name,
-                            training_image_path  = args.training_image_path,
-                            transform            = NormalizeImageDict(['Histo','MRI']))
+                            training_image_path  = args.training_image_path
+                            #transform            = NormalizeImageDict(['Histo','MRI'])
+                            )
 
 dataset_test = SynthDataset(geometric_model      = 'tps',
                             csv_file             = './training_data/tps/' + args.test_csv_name,
@@ -299,6 +190,7 @@ dataset_test = SynthDataset(geometric_model      = 'tps',
 
 dataloader_train = DataLoader(dataset_train, batch_size=args.batch_size, shuffle=True, num_workers=4)
 dataloader_test  = DataLoader(dataset_test,  batch_size=args.batch_size, shuffle=True, num_workers=4)
+
 
 pair_generation_tnf = SynthPairTnf(geometric_model='tps',use_cuda=use_cuda)
 landmark_tnf        = LandmarkTnf(use_cuda=use_cuda)
@@ -320,7 +212,7 @@ testLossArray   = np.zeros(args.num_epochs)
 for epoch in range(1, args.num_epochs+1):
     train_loss = train(epoch,model,loss,optimizer,dataloader_train,landmark_tnf)
     test_loss  = test(model,loss,dataloader_test,pair_generation_tnf,use_cuda=use_cuda, geometric_model='tps')
-
+    
     scheduler.step()
     
     epochArray[epoch-1]     = epoch
