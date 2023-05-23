84aaf833f22ed97bd92b11bcb4972c34e1245122
Martamasramon <Marta.masramon@gmail.com>
2023-05-23 23:49:41 +0100

    created custom mse loss

--- model/loss.py
+++ model/loss.py
@@ -10,19 +10,46 @@ from skimage import io
 class SSDLoss(nn.Module):
     def __init__(self, geometric_model='affine', use_cuda=True):
         super(SSDLoss, self).__init__()
-        self.geometric_model = geometric_model
-        self.use_cuda = use_cuda
+        self.geometric_model    = geometric_model
+        self.use_cuda           = use_cuda
 
     def forward(self, theta, theta_GT, tnf_batch):
         ### compute square root of ssd
-        A = tnf_batch['target_image']
-        geometricTnf = GeometricTnf(self.geometric_model, 240, 240, use_cuda = self.use_cuda)
+        geometricTnf = GeometricTnf(self.geometric_model, use_cuda = self.use_cuda)
         
+        A = tnf_batch['target_image']
         B = geometricTnf(tnf_batch['source_image'],theta)
         
         ssd = torch.sum(torch.sum(torch.sum(torch.pow(A - B,2),dim=3),dim=2),dim=1)
         ssd = torch.sum(ssd)/(A.shape[0]*A.shape[1]*A.shape[2]*A.shape[3])
         ssd = torch.sqrt(ssd)
-
         
         return  ssd 
+
+class MSELoss(nn.Module):
+    def __init__(self, geometric_model='affine', use_cuda=True):
+        super(MSELoss, self).__init__()
+        self.geometric_model    = geometric_model
+        self.use_cuda           = use_cuda
+
+    def forward(self, theta, tnf_batch):
+        # Assume batch_size = 1
+        geometricTnf = GeometricTnf(self.geometric_model, use_cuda=self.use_cuda)
+
+        A = tnf_batch['target_landmarks']
+        B = geometricTnf(tnf_batch['source_landmarks'], theta)
+        
+        # Warped landmarks shape is [N,num_landmarks,240,240]
+        # tnf_batch['target_landmarks'] shape is [N,num_landmarks,2]
+        _,length,_ = A.shape
+        
+        temp        = B.view(length, -1).argmax(1).view(-1, 1)
+        indices     = torch.cat((temp // 240, temp % 240), dim=1)
+        # Indices has shape [num_landmarks,2]
+        # Reshape A to same dimensions
+        A = A.view(length, -1) 
+    
+        mse = torch.mean((A - indices)*(A - indices), dim=0)
+        
+        return mse
+        
