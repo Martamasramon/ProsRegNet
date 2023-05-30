0606b55a25752156f646fc3e584890817ed692d8
martamasramon <marta.masramon@gmail.com>
2023-05-30 15:33:53 +0100

    additional fixes for t2-dwi registration

--- train_mri.sh
+++ train_mri.sh
@@ -15,6 +15,6 @@ date
 export PATH=/share/apps/python-3.6.9-tkinter/bin:$PATH
 export LD_LIBRARY_PATH=/share/apps/python-3.6.9-tkinter/lib:$LD_LIBRARY_PATH
 
-python3 train.py --geometric-model tps -n 'histo_mri' -t 'histo_mri_train.csv' -s 'histo_mri_test.csv' -p 'datasets/training'
+python3 train.py --geometric-model tps-mri -n 'mri-3x3' -t 'mri_train.csv' -s 'mri_test.csv' -p 'datasets/training'
 
 date
